589e19bee1cfe7b9f127bc0c526a01a1199134a2
Rachel Robson <rachel.robson@digital.hmrc.gov.uk>
2016-02-02 14:53:36 +0000

    new layout version

--- app/new-layout/app.js
+++ app/new-layout/app.js
@@ -60,40 +60,4 @@ app.get('/add-end-date', function (req, res) {
     });
 });
 
-
-
-app.post('/change-both-dates', function (req, res) {
-    req.session.startDay = req.body.startDay;
-    req.session.startMonth = req.body.startMonth;
-    req.session.startYear = req.body.startYear;
-    req.session.endDay = req.body.endDay;
-    req.session.endMonth = req.body.endMonth;
-    req.session.endYear = req.body.endYear;
-    res.redirect(req.body['next-page']);
-});
-
-app.get('/review-both', function (req, res) {
-    var fdate = moment(req.session.endYear + '-' + req.session.endMonth + '-' + req.session.endDay);
-    var date = moment(req.session.startYear + '-' + req.session.startMonth + '-' + req.session.startDay);
-    res.render('review-both', {
-        inputStartDate: date.format(format),
-        inputEndDate: fdate.format(format),
-        endDate: fdate.day(+6).format(format),
-        startDate: date.day(0).format(format)
-    });
-});
-
-app.get('/confirm-both', function (req, res) {
-    var fdate = moment(req.session.endYear + '-' + req.session.endMonth + '-' + req.session.endDay);
-    var date = moment(req.session.startYear + '-' + req.session.startMonth + '-' + req.session.startDay);
-    res.render('confirm-both', {
-        inputStartDate: date.format(format),
-        inputEndDate: fdate.format(format),
-        endDate: fdate.day(+6).format(format),
-        startDate: date.day(0).format(format)
-    });
-});
-
-
-
 module.exports = app;
